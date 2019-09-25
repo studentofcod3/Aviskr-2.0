10dd16b6ed80c6bdde71833590201b5ae8b2765a
ayyoub <ayyoub@hotmail.com>
2019-09-25 17:42:51 +0200

    Updates

---
+++ precache-manifest.be348e91465095d62774fa918cd107d3.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70bab8df63ed40265f7703c219ffc7a8",
+    "url": "/Aviskr-2.0/index.html"
+  },
+  {
+    "revision": "fc772fe42d96fc5fab41",
+    "url": "/Aviskr-2.0/static/css/main.5678b50d.chunk.css"
+  },
+  {
+    "revision": "ea5f7572821aaf146888",
+    "url": "/Aviskr-2.0/static/js/2.23466301.chunk.js"
+  },
+  {
+    "revision": "fc772fe42d96fc5fab41",
+    "url": "/Aviskr-2.0/static/js/main.13edbc1e.chunk.js"
+  },
+  {
+    "revision": "f68449dcdfefd0e46b01",
+    "url": "/Aviskr-2.0/static/js/runtime~main.5b71efb3.js"
+  },
+  {
+    "revision": "385591d48cff9e8ceaa039ff7aead149",
+    "url": "/Aviskr-2.0/static/media/dashboard-min.385591d4.JPG"
+  },
+  {
+    "revision": "5e2582288bc2bc8855e59d291ff6f2c4",
+    "url": "/Aviskr-2.0/static/media/dashboard.5e258228.JPG"
+  },
+  {
+    "revision": "b5fea1a0e3b8c776e73d1c0ed2cc8b49",
+    "url": "/Aviskr-2.0/static/media/dashboard2-min.b5fea1a0.jpg"
+  },
+  {
+    "revision": "6dd3137de682d8a8a63a4c1c1b13509c",
+    "url": "/Aviskr-2.0/static/media/dashboard2.6dd3137d.jpg"
+  },
+  {
+    "revision": "fb6cb90502d7467ba5701cbe8ce24c8a",
+    "url": "/Aviskr-2.0/static/media/drone-full-min.fb6cb905.jpg"
+  },
+  {
+    "revision": "4cabc9e6fa78b5dca5185ea181a73adc",
+    "url": "/Aviskr-2.0/static/media/drone-full.4cabc9e6.jpg"
+  },
+  {
+    "revision": "e2173a4dd05725fb4f971a81467cbb0d",
+    "url": "/Aviskr-2.0/static/media/drone-min.e2173a4d.jpg"
+  },
+  {
+    "revision": "b108ed46665982f7617d5eacb5c4ec95",
+    "url": "/Aviskr-2.0/static/media/drone.b108ed46.jpg"
+  },
+  {
+    "revision": "a11d419550d8bffa673d1a4f939e3f36",
+    "url": "/Aviskr-2.0/static/media/fig1.a11d4195.png"
+  },
+  {
+    "revision": "97b4a6a2d2a4135251aa13e49d1a4b54",
+    "url": "/Aviskr-2.0/static/media/fig2.97b4a6a2.png"
+  },
+  {
+    "revision": "c71f90cfd4c863b84d9f8b60c36ce521",
+    "url": "/Aviskr-2.0/static/media/fig3.c71f90cf.png"
+  },
+  {
+    "revision": "7bca25345a2c9c9b7e2f9db689e2dcd4",
+    "url": "/Aviskr-2.0/static/media/fig4.7bca2534.png"
+  },
+  {
+    "revision": "fc5ae6ce60287fa9a7fb804d5338b00f",
+    "url": "/Aviskr-2.0/static/media/hero-desktop.fc5ae6ce.gif"
+  },
+  {
+    "revision": "3359380363a9c0e2ff64ab8565ac127b",
+    "url": "/Aviskr-2.0/static/media/hero-phone.33593803.jpg"
+  }
+]);
